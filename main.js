85b16138efd1417e59a1cd64f04e39c3e543af47
Alicja <alanotowska@yahoo.co.uk>
2024-04-01 15:46:45 +0200

    Adss the basic A11Y Slider

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
     primaryHeader.toggleAttribute("data-overlay");
 });
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: false
+});
